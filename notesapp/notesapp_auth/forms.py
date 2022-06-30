d91e8fc3c1651b7f021752efbc25d0e04bcd07f0
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-06-30 10:32:10 -0500

    Profile view and Registration form complete

--- notesapp/notesapp_auth/forms.py
+++ notesapp/notesapp_auth/forms.py
@@ -3,3 +3,13 @@ from crispy_forms.layout import Submit
 from django_registration.forms import RegistrationForm
 
 from notesapp_auth.models import User
+
+
+class NotesappRegistrationForm(RegistrationForm):
+    class Meta(RegistrationForm.Meta):
+        model = User
+
+    def __init__(self, *args, **kwargs):
+        super(NotesappRegistrationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit("submit", "Register"))
