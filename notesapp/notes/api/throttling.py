8cdcc163dd0b31ed2c0bd3e5652c1cb1c796795c
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-07-10 08:47:51 -0500

    Integrate throttling

---
+++ notesapp/notes/api/throttling.py
@@ -0,0 +1,23 @@
+# Throttling
+# https://ilovedjango.com/django/rest-api-framework/throttling-in-django-rest-framework/
+
+# Custom Throttling Rules
+# https://www.django-rest-framework.org/api-guide/throttling/#custom-throttles
+
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+import random
+
+class AnonSustainedThrottle(AnonRateThrottle):
+    scope = "anon_sustained"
+
+
+class AnonBurstThrottle(AnonRateThrottle):
+    scope = "anon_burst"
+
+
+class UserSustainedThrottle(UserRateThrottle):
+    scope = "user_sustained"
+
+
+class UserBurstThrottle(UserRateThrottle):
+    scope = "user_burst"
