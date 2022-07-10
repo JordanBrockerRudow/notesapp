3bb893e406563e7ecd30d250107021c0697e7b33
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-07-10 08:48:39 -0500

    Finish url routing for entire app

--- notesapp/notesapp/urls.py
+++ notesapp/notesapp/urls.py
@@ -1,21 +1,37 @@
-"""notesapp URL Configuration
+import debug_toolbar
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+import notes.views
+
+import notesapp_auth.views
+from django_registration.backends.activation.views import RegistrationView
+from notesapp_auth.forms import NotesappRegistrationForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', notes.views.index),
+    path("post/<slug>/", notes.views.post_detail, name="notes-post-detail"),
+    path("ip/", notes.views.get_ip),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
+    path(
+    "accounts/register/",
+    RegistrationView.as_view(form_class=NotesappRegistrationForm),
+    name="django_registration_register",
+    ),
+    path("accounts/profile/", notesapp_auth.views.profile, name="profile"),
+    path("accounts/", include("django_registration.backends.activation.urls")),
+    path("api/v1/", include("notes.api.urls")),
+    path("post-table/", notes.views.post_table, name="notes-post-table"),
 ]
+
+# Map the path __debug__/ to the DJDT's URL's, but only in debug mode.
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
