45d431fbb872b9bf81c64ed82a06a73ab1c26043
--J <--JordanBrockerRudow@gmail.com>
2022-07-18 14:57:17 +0000

    Remove FBV for logout
    Co-Authored-By: Jordan Brocker Rudow <jbrockerrudow@gmail.com>

--- notesapp/notesapp_auth/views.py
+++ notesapp/notesapp_auth/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout as main_logout
+
 
 # Logged-in User View
 @login_required
 def profile(request):
     return render(request, "notesapp_auth/profile.html")
-
-
-def logout(*args, **kwargs):
-    resp = main_logout(*args, **kwargs)
-    # redirect to home page after 2 seconds
-    resp['Refresh'] = '2;URL=/'
-    return resp
