6503e9974474aea2538a9c5cc43ea6384746cdd3
--J <--JordanBrockerRudow@gmail.com>
2022-07-20 00:50:35 +0000

    Basic Index View for Flashcards & Register models
    Co-Authored-By: Jordan <jbrockerrudow@gmail.com>

--- notesapp/flashcards/admin.py
+++ notesapp/flashcards/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from flashcards.models import Card, Cardset
 
-# Register your models here.
+
+admin.site.register(Card)
+admin.site.register(Cardset)
--- notesapp/flashcards/views.py
+++ notesapp/flashcards/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your views here.
+from flashcards.models import Card, Cardset
+from flashcards.forms import CardForm
+
+import logging
+
+# Logging
+logger = logging.getLogger(__name__)
+
+# Main Page for Flashcards
+def flashcard_index(request):
+    # Returns Flashcard Question, Cardset, Datetime
+    cards = Card.objects.filter(published_at__lte=timezone.now()).select_related("cardsets")
+    # Log quantity of posts
+    logger.debug("Got %d flashcards", len(cards))
+
+    return render(request, "notes/index.html", {"cards": cards})
+
