410e9b095db25fdeb2f507507fae96b56e87bc61
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-07-21 09:42:06 -0500

    Begin notes api FBV

--- notesapp/notes_api/views.py
+++ notesapp/notes_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from django.views.generic.base import TemplateView
+
 import logging
-# Create your views here.
+
+
+
+class NotesAPIHome(TemplateView):
+
+    template_name = "notes_api/api-index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
