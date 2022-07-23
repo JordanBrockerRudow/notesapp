79f17f6ce691b279aefca7b159ad7ecf8245bb66
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-07-23 13:25:08 -0500

    Add flashcards app to project routing

--- notesapp/notesapp/urls.py
+++ notesapp/notesapp/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.conf.urls import url
 
 import notes.views
 
@@ -11,14 +12,14 @@ import notesapp_auth.views
 from django_registration.backends.activation.views import RegistrationView
 from notesapp_auth.forms import NotesappRegistrationForm
 
-import flashcards.views
+import flashcards
 import notes_api.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', notes.views.index, name="home"),
-    path('flashcards/', flashcards.views.FlashCardsIndexView.as_view(), name="flashcards-index"),
+    path('flashcards/', include('flashcards.urls')),
     path("note/<slug>/", notes.views.post_detail, name="notes-detail"),
     path("ip/", notes.views.get_ip),
     path("accounts/", include("django.contrib.auth.urls")),
