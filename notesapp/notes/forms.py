79f1d86a642b243b8c5272f3f7180a2a95030b31
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-07-23 13:15:36 -0500

    CRUD Forms for user view

--- notesapp/notes/forms.py
+++ notesapp/notes/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
-from notes.models import Comment
+from notes.models import Comment, Post, Tag
 
 
 class CommentForm(forms.ModelForm):
@@ -13,5 +13,26 @@ class CommentForm(forms.ModelForm):
 
   def __init__(self, *args, **kwargs):
     super(CommentForm, self).__init__(*args, **kwargs)
+    self.helper = FormHelper()
+    self.helper.add_input(Submit('submit', 'Submit'))
+
+class TagForm(forms.ModelForm):
+  class Meta:
+    model = Tag
+    fields = ["value"]
+
+  def __init__(self, *args, **kwargs):
+    super(TagForm, self).__init__(*args, **kwargs)
+    self.helper = FormHelper()
+    self.helper.add_input(Submit('submit', 'Submit'))
+
+
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ["title", "summary", "content", "tags", "hero_image"]
+
+  def __init__(self, *args, **kwargs):
+    super(PostForm, self).__init__(*args, **kwargs)
     self.helper = FormHelper()
     self.helper.add_input(Submit('submit', 'Submit'))
