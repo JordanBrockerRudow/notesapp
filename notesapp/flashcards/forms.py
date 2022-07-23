031690ab658493560c18b8a03821d5c9c5d5e9fb
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-07-23 13:16:31 -0500

    Create forms for cardsets

--- notesapp/flashcards/forms.py
+++ notesapp/flashcards/forms.py
@@ -3,8 +3,16 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
-from flashcards.models import Card
+from flashcards.models import Card, Cardset
+class CardSetForm(forms.ModelForm):
+  class Meta:
+    model = Cardset
+    fields = ["set_name"]
 
+  def __init__(self, *args, **kwargs):
+    super(CardSetForm, self).__init__(*args, **kwargs)
+    self.helper = FormHelper()
+    self.helper.add_input(Submit('submit', 'Submit'))
 
 class CardForm(forms.ModelForm):
   class Meta:
